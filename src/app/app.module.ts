0785c4b2b0d3a1067414e28ad373646d774bed13
schesnowitz <steve@chesnowitz.com>
2017-04-04 01:46:42 -0400

    components - bootstrap - some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ListingsComponent } from './components/listings/listings.component';
+import { ListingComponent } from './components/listing/listing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddListingComponent } from './components/add-listing/add-listing.component';
+import { EditListingComponent } from './components/edit-listing/edit-listing.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'listings', component: ListingsComponent},
+  {path: 'add-listing', component: AddListingComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListingsComponent,
+    NavbarComponent,
+    ListingComponent,
+    AddListingComponent,
+    EditListingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
